c063257609bcd3a3962492592538d6d928648bb7
milosevic85 <dm9010@student.uni-lj.si>
2024-03-13 15:55:26 +0100

    feature(dani-imdb): 01_Zacetek_implement_initial_setup
    Description:
    - Spring Boot DevTools, Spring Web, JPA and H2 Database, Docker Compose, Lombok
    
    Refers to: #01_Zacetek_implement_initial_setup

---
+++ src/main/java/com/naloga/daniimdb/DaniImdbApplication.java
@@ -0,0 +1,13 @@
+package com.naloga.daniimdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaniImdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaniImdbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/naloga/daniimdb/DaniImdbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.naloga.daniimdb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaniImdbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
